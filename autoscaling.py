353a9a0c28ed1d93e190bc9f35547cb5ea518ba3
Karl Grzeszczak <karl@karlgrz.com>
2015-11-10 15:14:18 -0600

    Update autoscaling.py with creation calls

--- autoscaling.py
+++ autoscaling.py
@@ -1,20 +1,56 @@
 #!/usr/bin/python
 
 import boto.ec2.autoscale
-import sys
+import argparse
+from boto.ec2.autoscale import LaunchConfiguration
+from boto.ec2.autoscale import AutoScalingGroup
 
-def main(argv):
-	desired_capacity = argv[1]
-	print 'desired_capacity = {}'.format(desired_capacity)
+def main():
+	parser = argparse.ArgumentParser(description='Description of your program')
+	parser.add_argument('-c','--capacity', help='Capacity to increase autoscaling group to', required=False)
+	parser.add_argument('-a','--action', help='create or capacity', required=True)
+	args = vars(parser.parse_args())
 
-	ec2 = boto.ec2.autoscale.connect_to_region('us-east-1')
+	if args['action'] == 'create':
+	    create()
+	elif args['action'] == 'capacity':
+		change_capacity(args['capacity'])
 
-	groups = ec2.get_all_groups()
+def change_capacity(capacity):
+	conn = boto.ec2.autoscale.connect_to_region('us-east-1')
+
+	groups = conn.get_all_groups()
 
 	my_group = [group for group in groups if group.name == 'karlgrz-karlgrzcluster01-autoscaling-group'][0]
-	ec2.set_desired_capacity(my_group.name, desired_capacity)
-	print my_group
-	print my_group.name
+
+	conn.set_desired_capacity(my_group.name, capacity)
+
+def create():
+	conn = boto.ec2.autoscale.connect_to_region('us-east-1')
+
+	groups = conn.get_all_groups()
+
+	my_group = [group for group in groups if group.name == 'karlgrz-karlgrzcluster01-autoscaling-group']
+
+	print 'creating launch configuration and autoscaling group'
+	lc = LaunchConfiguration(name='karlgrz-karlgrzcluster01-launch-configuration',
+						 image_id='ami-d50d74bf',
+                         key_name='karlgrz-karlgrzcluster01',
+                         instance_profile_name='karlgrz-services',
+                         instance_type='t1.micro',
+                         block_device_mappings=['/dev/sda1'],
+                         user_data='#!/bin/bash\ndocker run --name json-service -p 8888:8888 karlgrz/karlgrz-dummy-json-service',
+                         security_groups=['karlgrzcluster01-us-east-1b-host01','karlgrzcluster01-us-east-1d-host01','karlgrzcluster01','karlgrz'])
+	conn.create_launch_configuration(lc)
+
+	ag = AutoScalingGroup(group_name='karlgrz-karlgrzcluster01-autoscaling-group', load_balancers=['karlgrz-karlgrzcluster01'],
+                          availability_zones=['us-east-1b', 'us-east-1d'],
+                          vpc_zone_identifier=['subnet-320e3419','subnet-9fd7dcc6'],
+                          launch_config=lc, min_size=0, max_size=5,
+                          health_check_type='ELB',
+                          health_check_period=300,
+                          connection=conn)
+	conn.create_auto_scaling_group(ag)
 
 if __name__ == '__main__':
-	main(sys.argv)
+	main()
